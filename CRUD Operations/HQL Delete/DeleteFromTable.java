ee0ff802459c504521ef5d64e9320f4d82d59907
Phani Cherukuri <cherukuriphanichowdary@gmail.com>
2017-06-04 04:18:41 -0500

    Add files via upload

---
+++ CRUD Operations/HQL Delete/DeleteFromTable.java
@@ -0,0 +1,31 @@
+package beans;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteFromTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Configuration cfg = new Configuration();
+		cfg.configure("resources/hibernate.cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction t = s.beginTransaction();
+		
+		String hql = "delete Delete where id=1";
+		Query q = s.createQuery(hql);
+		int i = q.executeUpdate();
+		
+		t.commit();
+		System.out.println(i);
+		s.close();
+		sf.close();
+		
+
+	}
+
+}
