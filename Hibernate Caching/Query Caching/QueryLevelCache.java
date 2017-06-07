e634477fb51cdc35a3dbe8750c0d8512a333b31e
Phani Cherukuri <cherukuriphanichowdary@gmail.com>
2017-06-06 22:44:32 -0500

    Update QueryLevelCache.java

--- Hibernate Caching/Query Caching/QueryLevelCache.java
+++ Hibernate Caching/Query Caching/QueryLevelCache.java
@@ -12,12 +12,25 @@ public class QueryLevelCache {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Laptop laptop = null;
+	Laptop laptop = null;
     	Configuration cfg = new Configuration().configure().addAnnotatedClass(Laptop.class);
     	ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(cfg.getProperties()).buildServiceRegistry();
     	SessionFactory sf = cfg.buildSessionFactory(reg);
     	Session s = sf.openSession();
     	s.beginTransaction();
+//    	Laptop l1 = new Laptop(1, "A1", "DELL", 1926, "8GB,256GB,i7,13'");
+//    	Laptop l2 = new Laptop(2, "B1", "MacBook", 1926, "8GB,256GB,i7,13'");
+//    	Laptop l3 = new Laptop(3, "C1", "Surface", 1926, "8GB,256GB,i7,13'");
+//    	Laptop l4 = new Laptop(4, "D1", "Lenovo", 1926, "8GB,256GB,i7,13'");
+//    	Laptop l5 = new Laptop(5, "E1", "HP", 1926, "8GB,256GB,i7,13'");
+//    	
+//    	s.save(l1);
+//    	s.save(l2);
+//    	s.save(l3);
+//    	s.save(l4);
+//    	s.save(l5);
+// 	s.getTransaction().commit();
+    	   
     	Query q1 = s.createQuery("from Laptop where laptopid=1");
     	q1.setCacheable(true);
     	laptop = (Laptop)q1.uniqueResult();
